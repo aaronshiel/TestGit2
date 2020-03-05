f9c2a087b32bbc62c3942d4bb4584c2e8996da30
shiela <shiela@GATES.ecs.csus.edu>
2020-03-05 14:42:55 -0800

    yes

--- TestGit2/src/csc131/gittest/DevOne.java
+++ TestGit2/src/csc131/gittest/DevOne.java
@@ -4,4 +4,9 @@ public class DevOne {
 	public static void main(String args[]) {
 		System.out.println("Hello Git");
 	}
+	public double myAddition() {
+		double one = 1.0;
+		double two = 2.0;
+		return two;
+	}
 }
